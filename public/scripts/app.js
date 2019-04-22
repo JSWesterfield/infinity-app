e604068f1fab1cca3190d33b9a20414230ec2785
Jake Westerfield <jecobwesterfield@gmail.com>
2019-04-22 01:25:55 -0400

    adding to master

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log('App is running');
+
+// JSX - Javascript XML
+var template = <p>This is our JSX from app.js</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template);
