b2f0139769711ea2f0bb29ae6d6f6b6790b6749a
Jake Westerfield <jecobwesterfield@gmail.com>
2019-04-22 01:54:08 -0400

    added in babel compiled Javascript directly within the app.js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,12 @@
-console.log('App is running');
+console.log('App.js is running');
 
 // JSX - Javascript XML
-var template = <p>This is our JSX from app.js</p>;
+//var template = <h1 id="someid">Something new</h1>;
+var template = React.createElement(
+    'h1',
+    { id: "someid" },
+    "Something new"
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template);
+ReactDOM.render(template, appRoot);
